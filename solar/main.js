d8d9629dd96ca6818b1845b09c6d35ed8e5a8a07
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-07-17 15:20:47 +0200

    not done with the scene without planets yet

--- solar/main.js
+++ solar/main.js
@@ -1,29 +1,38 @@
 import './style.css'
 import * as THREE from "three";
+import getStarfield from '../getStarfield';
 
 
 //adding a renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth , window.innerHeight);
+renderer.setSize(window.innerWidth , window.outerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
 //making a camera
-const fov = 50;
-const aspect = window.innerWidth / window.innerHeight;
+const fov = 60;
+const aspect = window.innerWidth / window.outerHeight;
 const near = 0.1;
-const far = 100;
+const far = 700;
 
 const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-camera.postion = 2;
+camera.position.z = 2;
 
 //making a scene 
 const scene = new THREE.Scene();
+
+//adding stars to the 
+const stars = new getStarfield({numStars : 5000})
+scene.add(stars)
+
+scene.fog = new THREE.FogExp2(0x000000, 0.05); // Fog with exponential decay
+
 
 
 //rendering it in a function]
 function animate(){
   requestAnimationFrame(animate)
+  stars.rotation.y += 0.0001; // Rotate the starfield
   renderer.render(scene,camera);
 }
 
