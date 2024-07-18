13d0c7b07424ef646b40fff4b62be912607057e7
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-07-18 22:25:31 +0200

    added a function that disable zooming in and rotating future objects that would be made and zoomed in

--- solar/main.js
+++ solar/main.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from "three";
 import getStarfield from '../getStarfield';
 import createParticleSystem from '../particleSystem';
-
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 //adding a renderer
 const renderer = new THREE.WebGLRenderer();
@@ -34,6 +34,20 @@ scene.fog = new THREE.FogExp2(0x000000, 0.05); // Fog with exponential decay
 const particleColor = new THREE.Color("white");
 const particleSystem = createParticleSystem(200 , particleColor);
 scene.add(particleSystem);
+
+//making a function to stop zooming and in the canvas
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableRotate = false;
+controls.enableZoom = false;
+
+// this event listner prevent us from zooming in
+window.addEventListener('wheel', (event) => {
+  if (event.ctrlKey) {
+    event.preventDefault();  // Prevent zooming via Ctrl + Mouse Wheel
+  }
+}, { passive: false });
+
+
 
 
 //rendering it in a function]
