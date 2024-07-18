92af8d8fd459243d4bb45041724606b40f2ed452
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-07-18 21:52:37 +0200

    added the wavy effect

--- solar/main.js
+++ solar/main.js
@@ -2,9 +2,9 @@ import './style.css'
 import * as THREE from "three";
 import getStarfield from '../getStarfield';
 import createParticleSystem from '../particleSystem';
-import { EffectComposer } from 'three/examples/jsm/Addons.js';
-import { RenderPass } from 'three/examples/jsm/Addons.js';
-import { UnrealBloomPass } from 'three/examples/jsm/Addons.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 
 //adding a renderer
 const renderer = new THREE.WebGLRenderer();
@@ -35,9 +35,19 @@ const particleColor = new THREE.Color("white");
 const particleSystem = createParticleSystem(200 , particleColor);
 scene.add(particleSystem);
 
-//making a composer
+// adding a post processing setup
+ const composer = new EffectComposer(renderer);
+ composer.addPass(new RenderPass(scene,camera));
 
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth , window.outerHeight),
+  1.5, 0.4, 0.85
+);
+composer.addPass(bloomPass);
 
+//animate particle colors to create  a wavy effect
+let time = 0;
+const colors = particleSystem.geometry.attributes.color.array;
 
 
 //rendering it in a function]
@@ -45,7 +55,15 @@ function animate(){
   requestAnimationFrame(animate)
   stars.rotation.y += 0.0001; // Rotate the starfield
   particleSystem.rotation.z += 0.0001;
-  renderer.render(scene,camera);
+  time += 0.01;
+    for (let i = 0; i < colors.length; i += 3) {
+        const colorValue = (Math.sin(time + i * 0.01) + 1) / 2; // Generate a wave effect
+        colors[i] = colorValue;     // R
+        colors[i + 1] = colorValue; // G
+        colors[i + 2] = colorValue; // B
+    }
+    particleSystem.geometry.attributes.color.needsUpdate = true;
+    composer.render();
 }
 
 animate()
